da06bf98e1ae69df4bec1d4d9b9ba8d1c8f97ab5
james1821 <jamesespinosamark@.com>
2023-08-21 02:04:08 +0800

    changes margin-top in mobile

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -7,7 +7,7 @@ function Banner() {
   return (
     <header id='head' className='h-full bg-white ' >
         <div className='flex flex-col justify-center  items-center lg:w-1/2 lg:float-right relative z-10'> 
-            <h1 className='text-5xl mt-20 text-center '><span className='text-pink-500'>"Quality Products</span>, for everyone"</h1>
+            <h1 className='text-5xl mt-10 md:mt-20 text-center '><span className='text-pink-500'>"Quality Products</span>, for everyone"</h1>
             <div className='flex  justify-center  items-center w-1/2 lg:float-right relative z-10 my-10 md:gap-10 gap-5'> 
                  <img className='h-[80px]' src={Logo} alt="" />
                  <img className='h-[100px]' src={PasalubongLogo} alt="" />

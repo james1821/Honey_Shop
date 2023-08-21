101c547ebc692f9c0388e55d74050340f7f26754
james1821 <jamesespinosamark@.com>
2023-08-21 13:48:00 +0800

    changes font-size

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -4,16 +4,16 @@ import { Fade, Slide } from "../../node_modules/react-awesome-reveal";
 
 export default function Products({header, subtitle, Logo}) {
   return (
-    <div id='Products' className='h-100'>
+    <div id='Products' className='h-full'>
         <div className='flex flex-col md:flex-row justify-center items-center absolute mt-10'>
          
-                <h1 className='text-center text-5xl mt-10 p-5' >{header}</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio expedita architecto perspiciatis labore provident quaerat debitis voluptate ab aliquid inventore culpa quasi doloremque vel pariatur veritatis, fugit molestias? Voluptatem, ratione!</p>
+                <h1 className='text-center text-5xl md:text-[8rem]  pb-5' >{header}</h1>
+                <p className='text-center text-2xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio expedita architecto perspiciatis labore provident quaerat debitis voluptate ab aliquid inventore culpa quasi doloremque vel pariatur veritatis, fugit molestias? Voluptatem, ratione!</p>
            
         </div>
         <Slide
         direction='right'>
-        <img src={Logo} alt=""  className='h-72 w-auto float-right opacity-25 relative '/>
+        <img src={Logo} alt=""  className='h-[400px] w-auto float-right bottom-0 opacity-25 relative '/>
         </Slide>
 
 

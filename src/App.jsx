0aa46b6dd53cdecfd5610469e6b87a55f34504a0
james1821 <jamesespinosamark@.com>
2023-08-27 08:30:51 +0800

    added img of nuts products and added a Price lists of products

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Banner from './component/Banner'
 import Products from './component/Products'
 import PasalubongLogo from './assets/img/pasalubong_logo.png'
 import prodList from './component/Product-list/product-list'
+import PriceList from './component/PriceList'; 
+import priceListData from './component/Product-list/Prices'; 
 function App() {
 
 
@@ -14,8 +16,9 @@ function App() {
      <Banner/>
      <Products header={prodList.prodList[0].prodHead} Logo={PasalubongLogo} subtitle={prodList.prodList[0].prodLine}/>
      {/* <Home/> */}
-
+     <PriceList data={priceListData} />
     </>
+  
   )
 }
 

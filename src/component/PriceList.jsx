5ab2a52263b0e16902849d426d7c51ba14bec209
james1821 <jamesespinosamark@.com>
2023-08-29 15:17:54 +0800

    removed 'grams' added color green in prodName

--- src/component/PriceList.jsx
+++ src/component/PriceList.jsx
@@ -36,9 +36,9 @@ const PriceList = ({ data }) => {
             <div className='flex flex-col justify-center items-center md:flex-row'>
               <img className="h-[200px] my-5" src={product.prodImg} alt={product.prodName} />
               <div className='flex flex-col justify-center items-center text-2xl p-10 bg-white rounded'>
-                <h2>{product.prodName}</h2>
+                <h2 className='text-green-600'>{product.prodName}</h2>
                 {product.prodPrice && <p>Price: {product.prodPrice}</p>}
-                {product.prodGrams && <p>Grams: {product.prodGrams}</p>}
+                {product.prodGrams && <p>{product.prodGrams}</p>}
               </div>
             </div>
           </SwiperSlide>

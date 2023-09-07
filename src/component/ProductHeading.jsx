f60cfc0136f9678ce76163c4830bf26aee5eaf44
james1821 <jamesespinosamark@.com>
2023-09-07 22:00:07 +0800

    removed comment and added some react type animation and find us svg

--- src/component/ProductHeading.jsx
+++ src/component/ProductHeading.jsx
@@ -7,13 +7,12 @@ import { Fade, Slide } from "react-awesome-reveal";
 export default function Products({header, subtitle, Logo}) {
   return (
     <div id='Products' className='h-full lg:h-[500px] flex flex-col justify-center items-center ' >
-           <p className='text-center w-2/3 text-6xl px-10 hidden lg:block '>{subtitle}</p> 
-        <div className='flex flex-col lg:flex-row justify-center items-center    '>
+           
+        <div className='flex flex-col lg:flex-row justify-center items-center'>
+                
               <Slide direction='left'><img src={Logo} alt="" className='md:h-[250px] w-auto basis-1/2'/></Slide>
-             
              <Slide direction='right'> <img src="/img/Products-nuts.png" alt="" className='h-auto md:w-[600px] ' /></Slide>
              
-                <p className='text-center text-3xl mx-10 md:hidden '>{subtitle}</p>
            
         </div>
         {/* <Slide

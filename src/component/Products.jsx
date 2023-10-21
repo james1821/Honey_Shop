8c0b7855690e841b86c724f2a0f5e2c540fa7c59
james1821 <jamesespinosamark@.com>
2023-10-22 02:38:26 +0800

    added individual flowers for  transition design

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Fade } from "../../node_modules/react-awesome-reveal";
+import { Fade, Slide } from "../../node_modules/react-awesome-reveal";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 
@@ -7,16 +7,35 @@ export default function Products({ Logo, Product, paddingTop, data, handleClick,
   const containerStyle = {
     backgroundColor: Background 
   };
+  const images = [
+    '/img/Flowers/Flower1.png',
+    '/img/Flowers/Flower2.png',
+    '/img/Flowers/Flower3.png',
+    '/img/Flowers/Flower4.png',
+    '/img/Flowers/Flower5.png',
+
+  ];
+  
   return (
     <div style={containerStyle} id={id} className={`flex flex-col ${reverseRow} h-screen  ${paddingTop} `}>
       <div className={`h-full lg:h-[430px] md:w-1/2 flex flex-col justify-center items-center`}>
-        <div className='flex flex-col justify-center items-center px-2'>
+        <div className='flex flex-row justify-center items-center px-2'>
+         
+          <Slide>
+            <img src={images[1]} alt="" className='md:h-[300px] ml-5 md:ml-20 h-[100px]  md:w-full' />
+          </Slide>
+          <Slide>
+            <img src={images[2]} alt="" className='md:h-[300px] ml-1 md:ml-10 h-[100px] md:w-full' />
+          </Slide>
           <Fade>
-            <img src={Logo} alt="" className='md:h-[200px] w-auto basis-1/2 h-[100px]' />
+            <img src={images[0]} alt="" className='md:h-[300px] h-[100px] md:w-full' />
           </Fade>
-          <Fade>
-            <img src={Product} alt="" className='h-auto md:w-[600px]' />
-          </Fade>
+          <Slide direction='right'>
+            <img src={images[4]} alt="" className='md:h-[300px]  md:ml-[-2.5rem] h-[100px] md:w-full' />
+          </Slide>
+          <Slide direction='right'>
+            <img src={images[3]} alt="" className='md:h-[300px]  md:ml-[-1rem] h-[100px] md:w-full' />
+          </Slide>
         </div>
         <Fade>
           <a href="https://www.facebook.com/dalidapasalubong" target='_blank'>

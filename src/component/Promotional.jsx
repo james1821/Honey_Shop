321411c13b25be50ad2520cff17ff5a91b0a6b1a
james1821 <jamesespinosamark@.com>
2023-10-23 02:08:55 +0800

    previous version that uses flashy and complex animations removed, replaced with more simple design (fade animation)

--- src/component/Promotional.jsx
+++ src/component/Promotional.jsx
@@ -1,16 +1,12 @@
-import React, { useRef } from 'react';
-import { TypeAnimation } from 'react-type-animation';
-import { useInView } from 'react-intersection-observer';
+import React from 'react';
+import { Fade } from 'react-awesome-reveal';
 
 function Promotional() {
   const textStyle = {
     display: 'inline-block',
     background: 'rgba(0, 0, 0, 0.7)',
-    padding: '10px', 
+    padding: '10px',
   };
-
-  const [ref, inView] = useInView();
-  const textRef = useRef(null);
 
   return (
     <div id='Promotional' className="relative h-screen">
@@ -18,20 +14,12 @@ function Promotional() {
         <source src="/img/Video/works.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div ref={ref} className="absolute inset-0 flex items-center justify-center text-center text-[50px] md:text-[100px] text-white">
-        {inView ? (
-          <TypeAnimation
-            ref={textRef}
-            sequence={[
-              'Made with Passion and Dedication, a labor of love that shines through every detail.',
-              1000
-            ]}
-            wrapper="span"
-            speed={40}
-            style={textStyle}
-            repeat={Infinity}
-          />
-        ) : null}
+      <div className="absolute inset-0 flex items-center justify-center text-center text-[50px] md:text-[100px] text-white">
+        <Fade >
+          <span style={textStyle}>
+            Made with Passion and Dedication, a labor of love that shines through every detail.
+          </span>
+        </Fade>
       </div>
     </div>
   );

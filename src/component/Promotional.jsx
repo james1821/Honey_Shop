dee3870de2accdab5b7809f398db78314b8b3e6f
james1821 <jamesespinosamark@.com>
2023-10-23 00:33:35 +0800

    added an interaction observer library for react type animation so the type animation will only be  executed when the user is on the promotional comp.

--- src/component/Promotional.jsx
+++ src/component/Promotional.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import { useInView } from 'react-intersection-observer';
 
 function Promotional() {
+  const textStyle = {
+    display: 'inline-block',
+    background: 'rgba(0, 0, 0, 0.7)',
+    padding: '10px', 
+  };
+
+  const [ref, inView] = useInView();
+  const textRef = useRef(null);
+
   return (
     <div id='Promotional' className="relative h-screen">
       <video autoPlay loop muted className="absolute inset-0 w-full h-screen object-cover">
         <source src="/img/Video/works.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="text-pink-200 text-4xl  md:text-[100px] md:leading-[85px] font-bold text-center p-10 bg-black bg-opacity-30">
-          Made with Passion and Dedication, a labor of love that shines through every detail.
-        </div>
+      <div ref={ref} className="absolute inset-0 flex items-center justify-center text-center text-[50px] md:text-[100px] text-white">
+        {inView ? (
+          <TypeAnimation
+            ref={textRef}
+            sequence={[
+              'Made with Passion and Dedication, a labor of love that shines through every detail.',
+              1000
+            ]}
+            wrapper="span"
+            speed={40}
+            style={textStyle}
+            repeat={Infinity}
+          />
+        ) : null}
       </div>
     </div>
   );

106afd6b7d4b84852929312db7139d05cb2470f6
james1821 <jamesespinosamark@.com>
2023-10-26 23:31:21 +0800

    added hidden keys to vercel and  also updated this file

--- src/component/FacebookMessenger.jsx
+++ src/component/FacebookMessenger.jsx
@@ -3,8 +3,8 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 function FacebookMessenger() {
   return (
     <div>
-      <FacebookProvider appId={import.meta.env.VITE_REACT_APP_FACEBOOK_APP_ID} chatSupport>
-        <CustomChat pageId={import.meta.env.VITE_REACT_APP_FACEBOOK_PAGE_ID} minimized={true} />
+      <FacebookProvider appId={process.env.VITE_REACT_APP_FACEBOOK_APP_ID} chatSupport>
+        <CustomChat pageId={process.env.VITE_REACT_APP_FACEBOOK_PAGE_ID} minimized={true} />
       </FacebookProvider>
     </div>
   );

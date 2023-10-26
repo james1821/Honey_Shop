bcf19aa49389b3d06e3abcd21d7c67c0c71f8880
james1821 <jamesespinosamark@.com>
2023-10-26 23:09:12 +0800

    added app id and page id (hidden)

--- src/component/FacebookMessenger.jsx
+++ src/component/FacebookMessenger.jsx
@@ -1,14 +1,13 @@
-"use client";
 import { FacebookProvider, CustomChat } from 'react-facebook';
 
 function FacebookMessenger() {
   return (
     <div>
-        <FacebookProvider appId="123456789" chatSupport>
-        <CustomChat pageId="123456789" minimized={false}/>
-      </FacebookProvider>    
+      <FacebookProvider appId={import.meta.env.VITE_REACT_APP_FACEBOOK_APP_ID} chatSupport>
+        <CustomChat pageId={import.meta.env.VITE_REACT_APP_FACEBOOK_PAGE_ID} minimized={true} />
+      </FacebookProvider>
     </div>
-  )
+  );
 }
 
-export default FacebookMessenger
+export default FacebookMessenger;

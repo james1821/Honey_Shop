4471bee4da61c4f9f806223200f3ad2b0e0fa8fa
james1821 <jamesespinosamark@.com>
2023-10-28 02:52:30 +0800

    added custom text to messenger integration

--- src/component/FacebookMessenger.jsx
+++ src/component/FacebookMessenger.jsx
@@ -1,11 +1,22 @@
-
+import React from 'react';
 import { FacebookProvider, CustomChat } from 'react-facebook';
 
 function FacebookMessenger() {
   return (
     <div>
       <FacebookProvider appId="342145981684727" chatSupport>
-        <CustomChat pageId="105637449200736" minimized={true} />
+        <CustomChat
+          pageId="105637449200736"
+          minimized={true}
+          greetingDialogDisplay="hide"
+          greetingDialogDelay="30"
+        >
+          <div>
+            <div>
+              <p>I'm interested</p>
+            </div>
+          </div>
+        </CustomChat>
       </FacebookProvider>
     </div>
   );
